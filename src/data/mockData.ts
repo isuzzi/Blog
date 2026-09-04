b709bbe61085801e01fca21bee837d6528907970
isuzzi <isuzzi11@gmail.com>
2026-09-05 00:00:18 +0900

    feat: Markdown 문서 연결 완료 및 mockData 항목 수정

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -3,18 +3,18 @@ export const mockPosts = [
     id: 1,
     title: "React로 블로그 만들기",
     date: "2026-01-01",
-    content: "React로 블로그 만들기에 대한 내용을 작성하려고 합니다.",
+    slug: "hello-world",
   },
   {
     id: 2,
     title: "TpeScript 타입을 어떻게 설계해야할까?",
     date: "2026-01-02",
-    content: "TypeScript를 사용하면서 고민했던 타입 설계",
+    slug: "typescript",
   },
   {
     id: 3,
     title: "Router 라우팅 방법",
     date: "2026-01-02",
-    content: "Router 라우팅 방법에 대해서 알아보자",
+    slug: "howtorouting",
   },
 ];
--- src/posts/[id]/PostDetailPage.tsx
+++ src/posts/[id]/PostDetailPage.tsx
@@ -1,20 +1,44 @@
 import { useParams } from "react-router-dom";
+import ReactMarkdown from "react-markdown";
 import { mockPosts } from "../../data/mockData";
+
+const posts = import.meta.glob("../../blog/*.md", {
+  query: "?raw",
+
+  import: "default",
+
+  eager: true,
+});
 
 export default function PostDetailPage() {
   const { id } = useParams();
+
+  // mockPosts에서 현재 id에 해당하는 게시글 찾기
   const post = mockPosts.find((post) => post.id === Number(id));
+
+  // 게시글이 없으면 에러 화면
+  if (!post) {
+    return <div>Post not found</div>;
+  }
+
+  // Markdown 파일 찾기
+  const markdownPost = Object.entries(posts).find(([path]) =>
+    path.endsWith(`${post.id}.md`),
+  );
+
+  // Markdown 파일이 없으면 에러 화면
+  if (!markdownPost) {
+    return <div>Markdown not found</div>;
+  }
+  const markdown = markdownPost[1];
+
   return (
     <div>
-      <h1 className="text-xl font-bold">Post {post.id}</h1>
       <div className="border border-t border-gray-200 p-6">
-        {post && (
-          <article>
-            <h2>{post.title}</h2>
-            <p>{post.date}</p>
-            <p>{post.content}</p>
-          </article>
-        )}
+        <article className="prose">
+          <p>{post.date}</p>
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </article>
       </div>
     </div>
   );
